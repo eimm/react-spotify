70a655ec5f7589ba80ab0fa93a0580bb90e71b2a
Maksim Filimonov1 <maksim_filimonov1@epam.com>
2020-08-18 13:31:22 +0300

    added :parrot: on success authorization and autorization

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const config ={
+    appId: '3333222d641d4904b6ebf0d9622bc07b',
+    appKey: '70e3a608511a47f8a3bfcaf9f0723dfc',
+    redirectLink: 'http://localhost:3000',
+}
+
+export const links = {
+    authLink: `https://accounts.spotify.com/ru/authorize?client_id=${config.appId}&response_type=code&redirect_uri=${config.redirectLink}`,
+    tokenLink: `https://accounts.spotify.com/api/token`,
+}
